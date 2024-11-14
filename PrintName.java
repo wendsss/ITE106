32e49a7166684194b923896a9f2ba41ac05c2e28
wendsss <trishawendytiglao04@gmail.com>
2024-11-14 15:55:54 +0800

    Create PrintName.java

---
+++ PrintName.java
@@ -0,0 +1,11 @@
+import myPackage.myClass;
+
+public class PrintName
+{
+	public static void main(String args[])
+	{
+		String name = "GeeksforGeeks";
+		myClass obj= new myClass();
+		obj.getNames(name);
+	}
+}
